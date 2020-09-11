391e8ace3bcd37aac87e10f624be06e56c8607d8
Sirozu <43061026+Sirozu@users.noreply.github.com>
2020-09-11 09:56:08 +0300

    Add server app

---
+++ SocketClient/SocketClient/Classification.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Data;
+using System.IO.Ports;
+using System.Linq;
+using System.Xml;
+
+
+namespace ApplicationGI
+{
+    class Classification
+    {
+        public static SerialPort port = new System.IO.Ports.SerialPort("COM3", 9600, Parity.None, 8, StopBits.One);
+
+
+        private static readonly int NUMCLASS = 3;
+        private static readonly int NUMWORD = 60;
+        private static string[,] myCollection = new string[NUMCLASS, NUMWORD];
+        private static void InputPrerocessing()
+        {
+            var doc = new XmlDocument();
+            //doc.Load("C:/Users/Siroz/OneDrive/Диплом/SocketClient/SocketClient/VariationAnswer.xml");
+            doc.Load("C:/Users/HORIZON/OneDrive/Диплом/SocketClient/SocketClient/VariationAnswer.xml");
+            var docRoot = doc.DocumentElement;
+
+            //myCollection.Add("окна", docRoot["WindowGroup"].InnerText.ToLower().Split(new char[] { '\n', ',', '.' }).Where(x => !string.IsNullOrWhiteSpace(x)).ToArray());
+            //myCollection.Add("свет", docRoot["LightGroup"].InnerText.ToLower().Split(new char[] { '\n', ',', '.' }).Where(x => !string.IsNullOrWhiteSpace(x)).ToArray());
+            //var a = docRoot["WindowGroup"].InnerText.ToLower().Split(new char[] { '\n', ',', '.' }).Where(x => !string.IsNullOrWhiteSpace(x)).ToArray();
+            //"дела"
+            int j = 0;
+            var a = (docRoot["BuisnesGroup"].InnerText.ToLower().Split(new char[] { '\n', ',', '.' }).Where(x => !string.IsNullOrWhiteSpace(x)).ToArray());
+            for (int i = 0; i < a.Length; i++)
+            {
+                myCollection[j, i] = a[i];
+                if (a.Length < NUMWORD && i > a.Length)
+                {
+                    myCollection[j, i] = "-1";
+                }
+            }
+            //"приветствия"
+            j++;
+            a = (docRoot["HelloGroup"].InnerText.ToLower().Split(new char[] { '\n', ',', '.' }).Where(x => !string.IsNullOrWhiteSpace(x)).ToArray());
+            for (int i = 0; i < a.Length; i++)
+            {
+                myCollection[j, i] = a[i];
+                if (a.Length < NUMWORD && i > a.Length)
+                {
+                    myCollection[j, i] = "-1";
+                }
+            }
+            //("прощания"
+            j++;
+            a = (docRoot["GoodbyeGroup"].InnerText.ToLower().Split(new char[] { '\n', ',', '.' }).Where(x => !string.IsNullOrWhiteSpace(x)).ToArray());
+            for (int i = 0; i < a.Length; i++)
+            {
+                myCollection[j, i] = a[i];
+                if (a.Length < NUMWORD && i > a.Length)
+                {
+                    myCollection[j, i] = "-1";
+                }
+            }
+            //foreach (var x in myCollection)
+            //{
+            //    Console.WriteLine(x);
+            //}
+
+        }
+
+        private static string SelectAnswer(string a)
+        {
+            a.ToLower().Trim().Replace("!,.;:", "");
+            string result = "";
+            bool HELLOflag = false;
+            for (int i = 0; i < NUMWORD; i++)
+            {
+                if (myCollection[1, i] != "-1" && myCollection[1, i] != null)
+                {
+                    try
+                    {
+                        if (a.Contains(myCollection[1, i].Trim().Replace("!", "")))
+                        {
+                            HELLOflag = true;
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Error: " + e.Message);
+                    }
+                }
+            }
+            if (HELLOflag)
+            {
+                result += "Здравствуйте ";
+            }
+
+            bool GOODBYEflag = false;
+            for (int i = 0; i < NUMWORD; i++)
+            {
+                if (myCollection[2, i] != "-1" && myCollection[2, i] != null)
+                {
+                    try
+                    {
+                        if (a.Contains(myCollection[2, i].Trim().Replace("!", "")))
+                        {
+                            HELLOflag = true;
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Error: " + e.Message);
+                    }
+                }
+            }
+            if (GOODBYEflag)
+            {
+                result += "До свидания ";
+            }
+
+            if (a.Contains("открой окно") || a.Contains("Открой окно"))
+            {
+                result += "открываю ";
+                try
+                {
+                    if (!(port.IsOpen))
+                        port.Open();
+                    port.Write("2");
+                    Console.WriteLine(port.ReadLine());
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error opening/writing to serial port :: "
+                                    + ex.Message, "Error!");
+                }
+            }
+
+            if (a.Contains("закрой окно") || a.Contains("Закрой окно"))
+            {
+                result += "закрываю ";
+                try
+                {
+                    if (!(port.IsOpen))
+                        port.Open();
+                    port.Write("3");
+                    Console.WriteLine(port.ReadLine());
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error opening/writing to serial port :: "
+                                    + ex.Message, "Error!");
+                }
+            }
+
+            if (a.Contains("включи свет") || a.Contains("Включи свет") || a.Contains("Включи") || a.Contains("включи"))
+            {
+                result += "включаю ";
+                try
+                {
+                    if (!(port.IsOpen))
+                        port.Open();
+                    port.Write("1");
+                    Console.WriteLine(port.ReadLine());
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error opening/writing to serial port :: "
+                                    + ex.Message, "Error!");
+                }
+            }
+
+            if (a.Contains("выключи свет") || a.Contains("Выключи свет") || a.Contains("Выключить свет")
+                || a.Contains("Выключи") || a.Contains("Выключить") || a.Contains("выключить") || a.Contains("выключи"))
+            {
+                result += "выключаю ";
+                try
+                {
+                    if (!(port.IsOpen))
+                        port.Open();
+                    port.Write("0");
+                    Console.WriteLine(port.ReadLine());
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error opening/writing to serial port :: "
+                                    + ex.Message, "Error!");
+                }
+            }
+
+            //Console.WriteLine(result);
+            return result;
+        }
+
+
+
+        public static string SENDRECV(string a)
+        {
+            InputPrerocessing();
+
+
+
+
+
+            Console.WriteLine("Send: " + a);
+            // if (port.IsOpen)
+            //    port.Close();
+
+            return SelectAnswer(a);
+        }
+
+        //public Classification()
+        //{
+        //    InputPrerocessing();
+        //    Console.WriteLine(SelectAnswer("привет  открой окно"));
+        //}
+
+    }
+}
---
+++ SocketClient/SocketClient/Program.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace ApplicationGI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //new Classification();
+            new Server(59965);
+        }
+    }
+
+    public class Server
+    {
+        TcpListener Listener;
+        public Server(int Port)
+        {
+            Listener = new TcpListener(IPAddress.Any, Port);
+
+            Listener.Start();
+
+            while (true)
+            {
+                ThreadPool.QueueUserWorkItem(new WaitCallback(ClientThread), Listener.AcceptTcpClient());
+            }
+        }
+
+        static void ClientThread(Object StateInfo)
+        {
+            try
+            {
+                var client = new Client();
+                client.ClientRun((TcpClient)StateInfo);
+            }
+            catch (Exception ex)
+            {
+                ((TcpClient)StateInfo).Close();
+            }
+        }
+
+        ~Server()
+        {
+            if (Listener != null)
+            {
+                Listener.Stop();
+            }
+        }
+    }
+
+    public class Client
+    {
+        const int MAX_LENGTH_HEADER = 8192;
+
+        private void SendError(TcpClient Client, int Code)
+        {
+            string CodeStr = Code.ToString() + " " + ((HttpStatusCode)Code).ToString();
+            string Html = "<html><body><h1>" + CodeStr + "</h1></body></html>";
+            string Str = "HTTP/1.1 " + CodeStr + "\nContent-type: text/html\nContent-Length:" + Html.Length.ToString() + "\n\n" + Html;
+            byte[] Buffer = Encoding.UTF8.GetBytes(Str);
+            Client.GetStream().Write(Buffer, 0, Buffer.Length);
+            Client.Close();
+        }
+
+        public async void ClientRun(TcpClient Client)
+        {
+            Console.WriteLine("{1} : {0} Connect OK", DateTime.Now, Client.Client.RemoteEndPoint.ToString());
+
+            int len = 0;
+            var s = Client.GetStream();
+
+            var lenBuf = new Byte[4];
+            await s.ReadAsync(lenBuf, 0, 4);
+
+            len = BitConverter.ToInt32(lenBuf, 0);
+
+            string Request = "";
+            byte[] Buffer = new byte[8192];
+            int Count;
+            while (len > 0)
+            {
+                Count = await Client.GetStream().ReadAsync(Buffer, 0, Buffer.Length);
+                len -= Count;
+                Request += Encoding.UTF8.GetString(Buffer, 0, Count);
+            }
+
+            Console.WriteLine(Request);
+
+            string answer = Classification.SENDRECV(Request);
+            Console.WriteLine(answer);
+
+
+            var resultString = answer;
+
+            byte[] buffer = Encoding.UTF8.GetBytes(resultString);
+            var len1 = BitConverter.GetBytes(buffer.Length);
+
+            await s.WriteAsync(len1, 0, 4);
+            await s.WriteAsync(buffer, 0, buffer.Length);
+
+            //var RequestUri = Request.RegexParse(@"^\w+\s+([^\s]+)[^\s]*\s+HTTP/.*|").Split(' ').FirstOrDefault(x => x[0] == '/');
+
+            //if (string.IsNullOrEmpty(RequestUri))
+            //{
+            //    SendError(Client, 400);
+            //    return;
+            //}
+
+            //RequestUri = Uri.UnescapeDataString(RequestUri);
+
+            //if (RequestUri.IndexOf("..") >= 0)
+            //{
+            //    SendError(Client, 400);
+            //    return;
+            //}
+
+            //if (RequestUri.Length > 0)
+            //{
+            //    var split = RequestUri.Split('?').ToList();
+
+            //    if (Request.Contains("POST"))
+            //    {
+            //        var postData = Request.Split(new string[] { "\r\n\r\n" }, StringSplitOptions.None);
+            //        split.Insert(1, postData[1]);
+            //    }
+
+            //    var resultString = "Оно работает"; //Формируем ответ
+
+            //    if (string.IsNullOrEmpty(resultString))
+            //    {
+            //        SendError(Client, 404);
+            //        return;
+            //    }
+
+            //    string Headers = "HTTP/1.1 200 OK\nContent-Type: text/html\nContent-Length: " + resultString.Length + "\n\n";
+            //    byte[] HeadersBuffer = Encoding.UTF8.GetBytes(Headers);
+            //    Client.GetStream().Write(HeadersBuffer, 0, HeadersBuffer.Length);
+            //    using (var reader = new MemoryStream(Encoding.UTF8.GetBytes(resultString.ToString())))
+            //    {
+            //        while (reader.Position < reader.Length)
+            //        {
+            //            Count = reader.Read(Buffer, 0, Buffer.Length);
+            //            Client.GetStream().Write(Buffer, 0, Count);
+            //        }
+            //        Client.Close();
+            //    }
+            //}
+            Client.Close();
+        }
+    }
+
+    public static class ext
+    {
+        public static string RegexParse(this string data, string pattern)
+        {
+            var result = "";
+
+            var regex = new Regex(pattern);
+            if (regex.IsMatch(data)) result = regex.Match(data).ToString();
+
+            return result;
+        }
+    }
+}
